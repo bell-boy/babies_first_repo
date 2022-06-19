1d6a7e2e896256bc59955883d62daae028341820
bell-boy <107776086+bell-boy@users.noreply.github.com>
2022-06-18 19:17:29 -0500

    add some flair to test.cpp

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::string name_user; 
 	std::cin>>name_user;
-	std::cout<<"hi "<<name_user<<" my name is joshua bello, but i go by femi";
+	std::cout<<"hi "<<name_user<<" my name is joshua bello, but i go by femi\n";
+	std::cout<<"how are you\n?";
 	return 0;
 }
